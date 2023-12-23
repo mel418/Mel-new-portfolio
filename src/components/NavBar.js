6280e1560fb65805e3763ad3769afd4aa39306fc
Melody <82139031+mel418@users.noreply.github.com>
2023-12-22 19:19:14 -0800

    use state and effect for banner animation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -29,29 +29,29 @@ export const NavBar = () => {
 
     return (
         <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
-        <Container>
-            <Navbar.Brand href="#home">
-                <img src={MELODY} alt="Logo" />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav"> 
-                <span className="navbar-toggler-icon"></span>
-            </Navbar.Toggle>
-            <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-                <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-                <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-                <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
-            </Nav>
-            <span className="navbar-text">
-                <div className="social-icon">
-                    <a href="#"><img src={navIcon1} alt="" /></a>
-                    <a href="#"><img src={navIcon2} alt="" /></a>
-                    <a href="#"><img src={navIcon3} alt="" /></a>
-                </div>
-                <button className="vvd" onClick = {() => console.log('connect')}/><span>Let's connect</span>
-            </span>
-            </Navbar.Collapse>
-        </Container>
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img src={MELODY} alt="Logo" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"> 
+                    <span className="navbar-toggler-icon"></span>
+                </Navbar.Toggle>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+                        <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+                        <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+                    </Nav>
+                    <span className="navbar-text">
+                        <div className="social-icon">
+                            <a href="https://linkedin.com/in/melody-gatan"><img src={navIcon1} alt="" /></a>
+                            <a href="https://github.com/mel418"><img src={navIcon2} alt="" /></a>
+                            <a href="https://www.instagram.com/mel.oh.dee/"><img src={navIcon3} alt="" /></a>
+                        </div>
+                        <button className="vvd" onClick = {() => console.log('connect')}><span>Let's connect</span></button>
+                    </span>
+                </Navbar.Collapse>
+            </Container>
         </Navbar>
     )
 }
